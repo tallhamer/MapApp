29fe3f18a140750b8182f7e255467abc977d031b
tallhamer <mike.tallhamer@proton.me>
2025-03-31 13:59:02 -0600

    added a dicom model for syncing files and added it to the app
    old code commented out

--- map_app.py
+++ map_app.py
@@ -2,7 +2,7 @@ import sys
 
 import PySide6.QtCore as qtc
 import PySide6.QtWidgets as qtw
-import PySide6.QtGui as qtg
+# import PySide6.QtGui as qtg
 
 import pydicom
 import numpy as np
@@ -16,6 +16,9 @@ import vtk
 from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
 from ui.test_window import Ui_MainWindow
+from model.dicom import DicomFileSyncModel
+
+DFS = DicomFileSyncModel()
 
 obj2dcm_transform_map = {None: np.array([[1, 0, 0], # Identity
                                          [0, 1, 0],
@@ -44,96 +47,116 @@ obj2dcm_transform_map = {None: np.array([[1, 0, 0], # Identity
                                          ),
                          }
 
+def has_actor(renderer, actor_to_check):
+    """
+    Checks if a vtkRenderer contains a specific actor.
+
+    Args:
+        renderer: The vtkRenderer.
+        actor_to_check: The actor to check for.
+
+    Returns:
+        True if the renderer contains the actor, False otherwise.
+    """
+    actors = renderer.GetActors()
+    actors.InitTraversal()
+    current_actor = actors.GetNextActor()
+    while current_actor is not None:
+        if current_actor == actor_to_check:
+            return True
+        current_actor = actors.GetNextActor()
+    return False
+
 # Define helper functions
-def get_dcm_body_point_cloud(dcm_filename):
-    # Read DICOM Structure Set
-    ds = pydicom.dcmread(dcm_filename)
-
-    # Generate ROI Look Up Table using the ROI Number as the key
-    roi_lut = {}
-    for structure in ds.StructureSetROISequence:
-        # print(f'{structure.ROIName} ({structure.ROIName.lower()})',
-        #       structure.ROINumber
-        #       )
-        roi_lut[structure.ROINumber] = structure.ROIName.lower()
-
-    # Grab the Body structure points
-    body_contours = []
-    for roi in ds.ROIContourSequence:
-        if (roi.ReferencedROINumber in roi_lut) and \
-                roi_lut[roi.ReferencedROINumber] == 'body':
-            # print('Found Body')
-            for contour in roi.ContourSequence:
-                # print(contour.ContourData)
-                body_contours.append([[float(contour.ContourData[i]),
-                                       float(contour.ContourData[i + 1]),
-                                       float(contour.ContourData[i + 2])
-                                       ] \
-                                      for i in range(0,
-                                                     len(contour.ContourData),
-                                                     3
-                                                     )
-                                      ]
-                                     )
-
-    dcm_points = np.array([point for contour in body_contours \
-                           for point in contour
-                           ]
-                          )
-
-    dcm_colors = np.zeros(dcm_points.shape)
-    dcm_colors[:, :] = [0.0, 0.5, 0.0]
-
-    dcm_pcloud = o3d.geometry.PointCloud()
-    dcm_pcloud.points = o3d.utility.Vector3dVector(dcm_points)
-    dcm_pcloud.colors = o3d.utility.Vector3dVector(dcm_colors)
-    dcm_pcloud.estimate_normals()
-
-    return dcm_pcloud
-
-def pcloud_to_mesh(pcd, voxel_size=3, iso_level_percentile=5):
-    # Convet Open3D point cloud to numpy array
-    points = np.asarray(pcd.points)
-
-    # Compute the bounds of the point cloud
-    mins = pcd.get_min_bound()
-    maxs = pcd.get_max_bound()
-
-    # print(mins)
-    # print(maxs)
-
-    x = np.arange(mins[0], maxs[0], voxel_size)
-    y = np.arange(mins[1], maxs[1], voxel_size)
-    z = np.arange(mins[2], maxs[2], voxel_size)
-    x, y, z = np.meshgrid(x, y, z, indexing='ij')
-
-    # Create a KD-tree for efficient nearest neighbor search
-    tree = cKDTree(points)
-
-    # Compute the scalar field (distance to nearest point)
-    grid_points = np.vstack([x.ravel(), y.ravel(), z.ravel()]).T
-    print(f'Using {len(x.ravel())} grid_points for marching cubes')
-    distances, _ = tree.query(grid_points, workers=-1)
-    scalar_field = distances.reshape(x.shape)
-
-    # Determine the iso_level based on the percentile of distance
-    iso_level = np.percentile(distances, iso_level_percentile)
-
-    # Apply Marching Cubes
-    verts, faces, _, _ = measure.marching_cubes(scalar_field, level=iso_level)
-
-    # Scale and translate vertices back to original coordinate system
-    verts = verts * voxel_size + mins
-
-    # Create the mesh
-    mesh = o3d.geometry.TriangleMesh()
-    mesh.vertices = o3d.utility.Vector3dVector(verts)
-    mesh.triangles = o3d.utility.Vector3iVector(faces)
-
-    mesh.compute_vertex_normals()
-    mesh.compute_triangle_normals()
-
-    return mesh
+# def get_dcm_body_point_cloud(dcm_filename):
+#     # Read DICOM Structure Set
+#     ds = pydicom.dcmread(dcm_filename)
+#
+#     # Generate ROI Look Up Table using the ROI Number as the key
+#     roi_lut = {}
+#     for structure in ds.StructureSetROISequence:
+#         # print(f'{structure.ROIName} ({structure.ROIName.lower()})',
+#         #       structure.ROINumber
+#         #       )
+#         roi_lut[structure.ROINumber] = structure.ROIName.lower()
+#
+#     # Grab the Body structure points
+#     body_contours = []
+#     for roi in ds.ROIContourSequence:
+#         if (roi.ReferencedROINumber in roi_lut) and \
+#                 roi_lut[roi.ReferencedROINumber] == 'body':
+#             # print('Found Body')
+#             for contour in roi.ContourSequence:
+#                 # print(contour.ContourData)
+#                 body_contours.append([[float(contour.ContourData[i]),
+#                                        float(contour.ContourData[i + 1]),
+#                                        float(contour.ContourData[i + 2])
+#                                        ] \
+#                                       for i in range(0,
+#                                                      len(contour.ContourData),
+#                                                      3
+#                                                      )
+#                                       ]
+#                                      )
+#
+#     dcm_points = np.array([point for contour in body_contours \
+#                            for point in contour
+#                            ]
+#                           )
+#
+#     dcm_colors = np.zeros(dcm_points.shape)
+#     dcm_colors[:, :] = [0.0, 0.5, 0.0]
+#
+#     dcm_pcloud = o3d.geometry.PointCloud()
+#     dcm_pcloud.points = o3d.utility.Vector3dVector(dcm_points)
+#     dcm_pcloud.colors = o3d.utility.Vector3dVector(dcm_colors)
+#     dcm_pcloud.estimate_normals()
+#
+#     return dcm_pcloud
+#
+# def pcloud_to_mesh(pcd, voxel_size=3, iso_level_percentile=5):
+#     # Convet Open3D point cloud to numpy array
+#     points = np.asarray(pcd.points)
+#
+#     # Compute the bounds of the point cloud
+#     mins = pcd.get_min_bound()
+#     maxs = pcd.get_max_bound()
+#
+#     # print(mins)
+#     # print(maxs)
+#
+#     x = np.arange(mins[0], maxs[0], voxel_size)
+#     y = np.arange(mins[1], maxs[1], voxel_size)
+#     z = np.arange(mins[2], maxs[2], voxel_size)
+#     x, y, z = np.meshgrid(x, y, z, indexing='ij')
+#
+#     # Create a KD-tree for efficient nearest neighbor search
+#     tree = cKDTree(points)
+#
+#     # Compute the scalar field (distance to nearest point)
+#     grid_points = np.vstack([x.ravel(), y.ravel(), z.ravel()]).T
+#     print(f'Using {len(x.ravel())} grid_points for marching cubes')
+#     distances, _ = tree.query(grid_points, workers=-1)
+#     scalar_field = distances.reshape(x.shape)
+#
+#     # Determine the iso_level based on the percentile of distance
+#     iso_level = np.percentile(distances, iso_level_percentile)
+#
+#     # Apply Marching Cubes
+#     verts, faces, _, _ = measure.marching_cubes(scalar_field, level=iso_level)
+#
+#     # Scale and translate vertices back to original coordinate system
+#     verts = verts * voxel_size + mins
+#
+#     # Create the mesh
+#     mesh = o3d.geometry.TriangleMesh()
+#     mesh.vertices = o3d.utility.Vector3dVector(verts)
+#     mesh.triangles = o3d.utility.Vector3iVector(faces)
+#
+#     mesh.compute_vertex_normals()
+#     mesh.compute_triangle_normals()
+#
+#     return mesh
 
 class MainWindow(qtw.QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -161,6 +184,11 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
         self.w_cb_dcm_color.currentTextChanged.connect(self.dcmColorNameChanged)
         self.w_cb_dcm_color.setCurrentText('green')
         self.w_hs_dcm_transparency.valueChanged.connect(self.dcmTransparencyChanged)
+
+        self.w_le_dcm_plan_file.textChanged.connect(DFS.update_plan_file)
+        self.w_le_dcm_struct_file.textChanged.connect(DFS.update_structure_file)
+
+        DFS.vtk_body_actor_updated.connect(self.visualizeDicom)
 
         self.patient_orientation = None
 
@@ -200,35 +228,36 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
                                                       )
         if filename:
             self.w_le_dcm_plan_file.setText(filename)
-            ds = pydicom.dcmread(filename)
 
-            current_orientation = None
-            bypass = False
-
-            for setup in ds.PatientSetupSequence:
-                if current_orientation is None:
-                    current_orientation = setup.PatientPosition
-                elif current_orientation != setup.PatientPosition:
-                    print("There are multiple patient orientations reported in the DICOM Plan")
-                else:
-                    pass
-
-                if (self.patient_orientation is None) or \
-                        (self.patient_orientation != setup.PatientPosition and not bypass):
-                    print("In Bypass loop")
-                    if setup.PatientPosition == 'HFS':
-                        self.w_rb_hfs.setChecked(True)
-                    elif setup.PatientPosition == 'HFP':
-                        self.w_rb_hfp.setChecked(True)
-                    elif setup.PatientPosition == 'FFS':
-                        self.w_rb_ffs.setChecked(True)
-                    elif setup.PatientPosition == 'FFP':
-                        self.w_rb_ffp.setChecked(True)
-                    else:
-                        pass
-                    bypass = True
-                else:
-                    pass
+            # ds = pydicom.dcmread(filename)
+            #
+            # current_orientation = None
+            # bypass = False
+            #
+            # for setup in ds.PatientSetupSequence:
+            #     if current_orientation is None:
+            #         current_orientation = setup.PatientPosition
+            #     elif current_orientation != setup.PatientPosition:
+            #         print("There are multiple patient orientations reported in the DICOM Plan")
+            #     else:
+            #         pass
+            #
+            #     if (self.patient_orientation is None) or \
+            #             (self.patient_orientation != setup.PatientPosition and not bypass):
+            #         print("In Bypass loop")
+            #         if setup.PatientPosition == 'HFS':
+            #             self.w_rb_hfs.setChecked(True)
+            #         elif setup.PatientPosition == 'HFP':
+            #             self.w_rb_hfp.setChecked(True)
+            #         elif setup.PatientPosition == 'FFS':
+            #             self.w_rb_ffs.setChecked(True)
+            #         elif setup.PatientPosition == 'FFP':
+            #             self.w_rb_ffp.setChecked(True)
+            #         else:
+            #             pass
+            #         bypass = True
+            #     else:
+            #         pass
 
     def openDcmStructFile(self):
         print(">>openDcmStructFile")
@@ -238,48 +267,15 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
                                                       "DICOM Files (*.dcm)"
                                                       )
         if filename:
+            self.w_le_dcm_struct_file.setText(filename)
+
             # Load the DICOM mesh
-            self._loadDcmMesh(filename)
+            # self._loadDcmMesh(filename)
 
-    def _loadDcmMesh(self, dcm_filename):
-        print(">>_loadDcmMesh")
-        # DICOM Point Cloud and Surface Mesh
-        print('Reading in DICOM "Body" structure from DICOM file')
-        dcm_pcloud = get_dcm_body_point_cloud(dcm_filename)
-
-        print("Generating DICOM surface using marchine cubes.")
-        dcm_mesh = pcloud_to_mesh(dcm_pcloud, voxel_size=3, iso_level_percentile=3)
-        print('DICOM Surface complete')
-
-        self.w_le_dcm_struct_file.setText(dcm_filename)
-
-        #############################
-        # START: DICOM Surface Data #
-        #############################
-
-        colors = vtk.vtkNamedColors()
-
-        dcm_verts = vtk.vtkPoints()
-        for vert in dcm_mesh.vertices:
-            dcm_verts.InsertNextPoint(*vert)
-
-        # Create a polydata object and add the points
-        self.dcm_polydata = vtk.vtkPolyData()
-        self.dcm_polydata.points = dcm_verts
-
-        # Create a vertex cell array to hold the triagles
-        dcm_triangles = vtk.vtkCellArray()
-        for i in range(len(dcm_mesh.triangles)):
-            dcm_triangles.InsertNextCell(3, dcm_mesh.triangles[i])
-        self.dcm_polydata.polys = dcm_triangles
-
-        # Create a mapper and actor
-        dcm_mesh_mapper = vtk.vtkPolyDataMapper()
-        self.dcm_polydata >> dcm_mesh_mapper
-
+    def visualizeDicom(self):
         if self.dcm_actor is None:
-            # Create the scene actor that represents the point cloud
-            self.dcm_actor = vtk.vtkActor(mapper=dcm_mesh_mapper)
+            self.dcm_actor = DFS.dcm_body_actor
+
             R, G, B = self.named_colors.GetColor3ub(self.w_cb_dcm_color.currentText())
             self.dcm_actor.GetProperty().SetColor(R / 255.0, G / 255.0, B / 255.0)
             self.dcm_actor.property.opacity = self.w_hs_dcm_transparency.value() / 100.0
@@ -288,16 +284,75 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
             self.vtk_renderer.ResetCamera()
         else:
             self.vtk_renderer.RemoveActor(self.dcm_actor)
+            self.dcm_actor = DFS.dcm_body_actor
 
-            # Create the scene actor that represents the point cloud
-            self.dcm_actor = vtk.vtkActor(mapper=dcm_mesh_mapper)
-            self.dcm_actor.property.color = colors.GetColor3d('Green')
+            R, G, B = self.named_colors.GetColor3ub(self.w_cb_dcm_color.currentText())
+            self.dcm_actor.GetProperty().SetColor(R / 255.0, G / 255.0, B / 255.0)
             self.dcm_actor.property.opacity = self.w_hs_dcm_transparency.value() / 100.0
 
             self.vtk_renderer.AddActor(self.dcm_actor)
             self.vtk_renderer.ResetCamera()
 
         self.vtk_render_window.Render()
+
+
+    # def _loadDcmMesh(self, dcm_filename):
+    #     print(">>_loadDcmMesh")
+    #     # DICOM Point Cloud and Surface Mesh
+    #     print('Reading in DICOM "Body" structure from DICOM file')
+    #     dcm_pcloud = get_dcm_body_point_cloud(dcm_filename)
+    #
+    #     print("Generating DICOM surface using marchine cubes.")
+    #     dcm_mesh = pcloud_to_mesh(dcm_pcloud, voxel_size=3, iso_level_percentile=3)
+    #     print('DICOM Surface complete')
+    #
+    #     self.w_le_dcm_struct_file.setText(dcm_filename)
+    #
+    #     #############################
+    #     # START: DICOM Surface Data #
+    #     #############################
+    #
+    #     colors = vtk.vtkNamedColors()
+    #
+    #     dcm_verts = vtk.vtkPoints()
+    #     for vert in dcm_mesh.vertices:
+    #         dcm_verts.InsertNextPoint(*vert)
+    #
+    #     # Create a polydata object and add the points
+    #     self.dcm_polydata = vtk.vtkPolyData()
+    #     self.dcm_polydata.points = dcm_verts
+    #
+    #     # Create a vertex cell array to hold the triagles
+    #     dcm_triangles = vtk.vtkCellArray()
+    #     for i in range(len(dcm_mesh.triangles)):
+    #         dcm_triangles.InsertNextCell(3, dcm_mesh.triangles[i])
+    #     self.dcm_polydata.polys = dcm_triangles
+    #
+    #     # Create a mapper and actor
+    #     dcm_mesh_mapper = vtk.vtkPolyDataMapper()
+    #     self.dcm_polydata >> dcm_mesh_mapper
+    #
+    #     if self.dcm_actor is None:
+    #         # Create the scene actor that represents the point cloud
+    #         self.dcm_actor = vtk.vtkActor(mapper=dcm_mesh_mapper)
+    #         R, G, B = self.named_colors.GetColor3ub(self.w_cb_dcm_color.currentText())
+    #         self.dcm_actor.GetProperty().SetColor(R / 255.0, G / 255.0, B / 255.0)
+    #         self.dcm_actor.property.opacity = self.w_hs_dcm_transparency.value() / 100.0
+    #
+    #         self.vtk_renderer.AddActor(self.dcm_actor)
+    #         self.vtk_renderer.ResetCamera()
+    #     else:
+    #         self.vtk_renderer.RemoveActor(self.dcm_actor)
+    #
+    #         # Create the scene actor that represents the point cloud
+    #         self.dcm_actor = vtk.vtkActor(mapper=dcm_mesh_mapper)
+    #         self.dcm_actor.property.color = colors.GetColor3d('Green')
+    #         self.dcm_actor.property.opacity = self.w_hs_dcm_transparency.value() / 100.0
+    #
+    #         self.vtk_renderer.AddActor(self.dcm_actor)
+    #         self.vtk_renderer.ResetCamera()
+    #
+    #     self.vtk_render_window.Render()
 
     def openObjFile(self):
         print(">>openObjFile")
---
+++ model/dicom.py
@@ -0,0 +1,179 @@
+import pydicom
+import numpy as np
+import open3d as o3d
+from skimage import measure
+from scipy.spatial import cKDTree
+
+import vtk
+from vtkmodules.util import numpy_support
+
+import PySide6.QtCore as qtc
+
+class DicomFileSyncModel(qtc.QObject):
+
+    plan_file_changed = qtc.Signal(str)
+    patient_orientation_changed = qtc.Signal(str)
+    structure_file_changed = qtc.Signal(str)
+    vtk_body_actor_updated = qtc.Signal()
+
+    def __init__(self):
+        super().__init__()
+        self._plan_file = None
+        self.patient_orientation = None
+        self.plan_file_changed.connect(self.update_plan_file)
+
+        self._structure_file = None
+        self.structure_file_changed.connect(self.update_structure_file)
+
+    @property
+    def plan_file(self):
+        return self._plan_file
+
+    @plan_file.setter
+    def plan_file(self, new_path):
+        self.plan_file_changed.emit(new_path)
+
+    @property
+    def structure_file(self):
+        return self._structure_file
+
+    @structure_file.setter
+    def structure_file(self, new_path):
+        self.structure_file_changed.emit(new_path)
+
+    qtc.Slot(str)
+    def update_plan_file(self, new_path):
+        print("In DicomFileSyncModel update_plan_file Slot")
+        self._plan_file = new_path
+        ds = pydicom.dcmread(self.plan_file)
+
+        for i in range(len(ds.PatientSetupSequence)):
+            orientation = ds.PatientSetupSequence[i].PatientPosition
+
+            if (self.patient_orientation is None) or (i == 0):
+                self.patient_orientation = orientation
+                self.patient_orientation_changed.emit(orientation)
+            elif self.patient_orientation != orientation:
+                print("There are multiple patient orientations reported in the DICOM Plan")
+            else:
+                pass
+
+    def _get_body_point_cloud(self):
+        # Read DICOM Structure Set
+        ds = pydicom.dcmread(self.structure_file)
+
+        # Generate ROI Look Up Table using the ROI Number as the key
+        roi_lut = {}
+        for structure in ds.StructureSetROISequence:
+            # print(f'{structure.ROIName} ({structure.ROIName.lower()})',
+            #       structure.ROINumber
+            #       )
+            roi_lut[structure.ROINumber] = structure.ROIName.lower()
+
+        # Grab the Body structure points
+        body_contours = []
+        for roi in ds.ROIContourSequence:
+            if (roi.ReferencedROINumber in roi_lut) and \
+                    roi_lut[roi.ReferencedROINumber] == 'body':
+                # print('Found Body')
+                for contour in roi.ContourSequence:
+                    # print(contour.ContourData)
+                    body_contours.append([[float(contour.ContourData[i]),
+                                           float(contour.ContourData[i + 1]),
+                                           float(contour.ContourData[i + 2])
+                                           ] \
+                                          for i in range(0,
+                                                         len(contour.ContourData),
+                                                         3
+                                                         )
+                                          ]
+                                         )
+
+        dcm_points = np.array([point for contour in body_contours \
+                               for point in contour
+                               ]
+                              )
+
+        dcm_pcloud = o3d.geometry.PointCloud()
+        dcm_pcloud.points = o3d.utility.Vector3dVector(dcm_points)
+        dcm_pcloud.estimate_normals()
+
+        return dcm_pcloud
+
+    def _pcloud_to_mesh(self, pcd, voxel_size=3, iso_level_percentile=5):
+        # Convet Open3D point cloud to numpy array
+        points = np.asarray(pcd.points)
+
+        # Compute the bounds of the point cloud
+        mins = pcd.get_min_bound()
+        maxs = pcd.get_max_bound()
+
+        # print(mins)
+        # print(maxs)
+
+        x = np.arange(mins[0], maxs[0], voxel_size)
+        y = np.arange(mins[1], maxs[1], voxel_size)
+        z = np.arange(mins[2], maxs[2], voxel_size)
+        x, y, z = np.meshgrid(x, y, z, indexing='ij')
+
+        # Create a KD-tree for efficient nearest neighbor search
+        tree = cKDTree(points)
+
+        # Compute the scalar field (distance to nearest point)
+        grid_points = np.vstack([x.ravel(), y.ravel(), z.ravel()]).T
+        print(f'Using {len(x.ravel())} grid_points for marching cubes')
+        distances, _ = tree.query(grid_points, workers=-1)
+        scalar_field = distances.reshape(x.shape)
+
+        # Determine the iso_level based on the percentile of distance
+        iso_level = np.percentile(distances, iso_level_percentile)
+
+        # Apply Marching Cubes
+        verts, faces, _, _ = measure.marching_cubes(scalar_field, level=iso_level)
+
+        # Scale and translate vertices back to original coordinate system
+        verts = verts * voxel_size + mins
+
+        # Create the mesh
+        mesh = o3d.geometry.TriangleMesh()
+        mesh.vertices = o3d.utility.Vector3dVector(verts)
+        mesh.triangles = o3d.utility.Vector3iVector(faces)
+
+        mesh.compute_vertex_normals()
+        mesh.compute_triangle_normals()
+
+        return mesh
+
+    qtc.Slot(str)
+    def update_structure_file(self, new_path):
+        print("In DicomFileSyncModel update_structure_file Slot")
+        self._structure_file = new_path
+
+        print('Reading in DICOM "Body" structure from DICOM file')
+        dcm_pcloud = self._get_body_point_cloud()
+
+        print("Generating DICOM surface using marchine cubes.")
+        dcm_mesh = self._pcloud_to_mesh(dcm_pcloud, voxel_size=3, iso_level_percentile=3)
+        print('DICOM Surface complete')
+
+        # Create a polydata object and add the points
+        self.dcm_polydata = vtk.vtkPolyData()
+        self.dcm_polydata.points = numpy_support.numpy_to_vtk(dcm_mesh.vertices)
+
+        # Create a vertex cell array to hold the triagles
+        dcm_triangles = vtk.vtkCellArray()
+        for i in range(len(dcm_mesh.triangles)):
+            dcm_triangles.InsertNextCell(3, dcm_mesh.triangles[i])
+        self.dcm_polydata.polys = dcm_triangles
+
+        # Create a mapper and actor
+        dcm_mesh_mapper = vtk.vtkPolyDataMapper()
+        self.dcm_polydata >> dcm_mesh_mapper
+
+
+        # Create the scene actor that represents the point cloud
+        self.dcn_body_actor = vtk.vtkActor(mapper=dcm_mesh_mapper)
+        self.dcm_body_actor.GetProperty().SetColor(0, 1, 0)
+        self.dcm_body_actor.property.opacity = 1
+
+        self.vtk_body_actor_updated.emit()
