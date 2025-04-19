bbd19a59e71b46eceb6ae7952fcfcc515eb4ac86
tallhamer <mike.tallhamer@proton.me>
2025-04-18 19:35:22 -0600

    added function print statements to track call structure

--- diag_orient.py
+++ diag_orient.py
@@ -4,6 +4,7 @@ from ui.obj_orientation_dialog import Ui_OrientationDialog
 
 class OrientDialog(qtw.QDialog, Ui_OrientationDialog):
     def __init__(self):
+        print('OrientDialog.__init__')
         super().__init__()
         self.setupUi(self)
 
